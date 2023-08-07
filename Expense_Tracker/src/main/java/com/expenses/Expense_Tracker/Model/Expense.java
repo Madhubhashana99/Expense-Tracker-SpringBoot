bba86e004e42535c491d4e1b8bec04b3a8cd9aa1
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-07 22:49:02 +0530

    update entities

---
+++ Expense_Tracker/src/main/java/com/expenses/Expense_Tracker/Model/Expense.java
@@ -0,0 +1,76 @@
+package com.expenses.Expense_Tracker.Model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="expences")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ExpenseCategory category;
+
+    @Column(nullable = false)
+    private String description;
+
+    public Expense(Long id, LocalDate date, BigDecimal amount, ExpenseCategory category, String description) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public ExpenseCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ExpenseCategory category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Expense_Tracker/src/main/java/com/expenses/Expense_Tracker/Model/ExpenseCategory.java
@@ -0,0 +1,36 @@
+package com.expenses.Expense_Tracker.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "expense_categories")
+public class ExpenseCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public ExpenseCategory(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
